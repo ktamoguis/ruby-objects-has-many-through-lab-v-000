412ec35d1ce29a895be8f1a856e5b10246c824aa
Karl Amoguis <ktamoguis@gmail.com>
2018-05-05 13:43:59 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,13 +1,11 @@
 class appointment
-  attr_accessor :date, :doctor
+  attr_accessor :date, :doctor, :patient
 
   def initialize(date, doctor)
     @date = date
     @doctor = doctor
   end
 
-  def patient
-    @patient = patient
-  end
+
 
 end
